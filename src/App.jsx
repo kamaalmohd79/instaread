e6e8f89a32c9f88ba645dc12f9a880e57d434754
kam <kamaalmohd79@gmail.com>
2022-05-08 01:58:38 +0530

    fixed header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './components/header';
+import Header from '../src/components/Header';
 import Hero from './components/hero';
 import Menu from './components/menu';
 
